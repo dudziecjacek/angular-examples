de5bc96cddff68ea3b965dc321125f9da4fe515e
jdudziec <dudziecjacek@gmail.com>
2021-07-09 13:40:33 +0200

    reactive forms with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<h1>Form validation example</h1>
+             <app-hero-form-template></app-hero-form-template>
+             <hr>
+             <app-hero-form-reactive></app-hero-form-reactive>`
 })
-export class AppComponent {
-  title = 'angular-examples';
-}
+export class AppComponent { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
